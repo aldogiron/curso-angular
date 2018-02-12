996fdd64034565b67e4b6b55c66c0fea193cbb4c
Miguel Roman <miguel.roman@autoweb.com>
2018-02-12 14:37:27 -0600

    formulario basico

--- apps/test-app/src/app/app.component.ts
+++ apps/test-app/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  private lastId = 0;
+
+  @ViewChild('firstInput')
+  firstInput: ElementRef;
+
+  title = 'angular';
+  nombre = '';
+  items = [];
+
+  ngOnInit() {
+    this.focusFirstInput();
+  }
+
+  submit() {
+
+    if (!this.nombre || !this.nombre.length) {
+      return;
+    }
+
+    this.lastId++;
+    const item = {
+      id: this.lastId,
+      nombre: this.nombre
+    };
+
+    this.items.push(item);
+    this.nombre = '';
+    this.focusFirstInput();
+  }
+
+  private focusFirstInput() {
+    const input = this.firstInput.nativeElement as HTMLInputElement;
+    input.focus();
+  }
 }
