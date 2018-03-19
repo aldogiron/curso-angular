b5a06bf688aa3e2a8798858a67ee073f9ac04088
Miguel Roman <miguelerm@gmail.com>
2018-03-18 23:53:42 -0600

    Refactor del RegistroUsuariosController

--- src/backend/Backend.Api/Controllers/RegistroUsuariosController.cs
+++ src/backend/Backend.Api/Controllers/RegistroUsuariosController.cs
@@ -4,6 +4,7 @@ using System.Transactions;
 using Backend.Api.Entities;
 using Backend.Api.Models;
 using Backend.Api.Services;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Api.Controllers
@@ -28,10 +29,31 @@ namespace Backend.Api.Controllers
                 return BadRequest(ModelState);
             }
 
+            var usuario = CrearUsuario(requestModel);
+            var responseModel = CrearResponseModel(usuario);
+            var url = "/api/usuarios/" + responseModel.Id;
+
+            return Created(url, responseModel);
+        }
+
+        private static RegistroUsuarioResponseModel CrearResponseModel(Usuario usuario)
+        {
+            return new RegistroUsuarioResponseModel
+            {
+                Id = usuario.Id,
+                Login = usuario.Login,
+                Correo = usuario.Correo,
+                Nombre = usuario.Nombre
+            };
+        }
+
+        private Usuario CrearUsuario(RegistroUsuarioRequestModel requestModel)
+        {
             var login = requestModel.Login.Trim().ToLower();
             var correo = requestModel.Correo.Trim().ToLower();
             var salt = hasher.CreateRandomSalt();
             var clave = hasher.ComputeHash(requestModel.Clave, salt);
+            var nombre = requestModel.Nombre.Trim();
 
             var usuario = new Usuario
             {
@@ -40,33 +62,28 @@ namespace Backend.Api.Controllers
                 Clave = clave,
                 Salt = salt,
                 Correo = correo,
-                Nombre = requestModel.Nombre.Trim()
+                Nombre = nombre
             };
+            
+            InsertarUsuario(usuario);
 
+            return usuario;
+        }
 
-            using(var trx = db.Database.BeginTransaction())
+        private void InsertarUsuario(Usuario usuario)
+        {
+            using (var trx = db.Database.BeginTransaction())
             {
-                var existe = db.Usuarios.Any(x => x.Login.ToLower() == login || x.Correo == correo);
+                var existe = db.Usuarios.Any(x => x.Login.ToLower() == usuario.Login || x.Correo == usuario.Correo);
                 if (existe)
                 {
-                    return BadRequest(new { Message = "Ya existe un usuario con ese login o correo registrado."});
+                    throw new ValidationException("Ya existe un usuario con ese login o correo registrado.");
                 }
-                
+
                 db.Usuarios.Add(usuario);
                 db.SaveChanges();
                 trx.Commit();
             }
-
-            var responseModel = new RegistroUsuarioResponseModel
-            {
-                Id = usuario.Id,
-                Login = usuario.Login,
-                Correo = usuario.Correo,
-                Nombre = usuario.Nombre
-            };
-
-            return Created("/api/usuarios/" + usuario.Id, responseModel);
         }
-
     }
 }
